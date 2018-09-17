b0ca2fc2dae9149475f241f14a6d6728e41fae79
tangwanggong <tangwanggong@163.com>
2018-09-17 17:28:29 +0800

    2018-09-17

---
+++ src/easy/array/ContainsDuplicate.java
@@ -0,0 +1,39 @@
+package easy.array;
+
+import java.util.Arrays;
+
+/**
+ * @author: Tang
+ * @create: 2018/9/17 17:24
+ **/
+public class ContainsDuplicate {
+    /**
+     * Given an array of integers, find if the array contains any duplicates.
+     *
+     * Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+     *
+     * Example 1:
+     *
+     * Input: [1,2,3,1]
+     * Output: true
+     * Example 2:
+     *
+     * Input: [1,2,3,4]
+     * Output: false
+     * Example 3:
+     *
+     * Input: [1,1,1,3,3,4,3,2,4,2]
+     * Output: true
+     * @param nums
+     * @return
+     */
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-1; i++) {
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/easy/string/ValidAnagram.java
@@ -0,0 +1,52 @@
+package easy.string;
+
+import java.util.Arrays;
+
+/**
+ * @author: Tang
+ * @create: 2018/9/17 17:12
+ **/
+public class ValidAnagram {
+
+    /**
+     * Given two strings s and t , write a function to determine if t is an anagram of s.
+     *
+     * Example 1:
+     *
+     * Input: s = "anagram", t = "nagaram"
+     * Output: true
+     * Example 2:
+     *
+     * Input: s = "rat", t = "car"
+     * Output: false
+     * Note:
+     * You may assume the string contains only lowercase alphabets.
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s==null&&t==null){
+            return true;
+        }
+        if(s==null&&t!=null){
+            return false;
+        }
+        if(t==null&&s!=null){
+            return false;
+        }
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        if(sChars.length!=tChars.length){
+            return false;
+        }
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        for (int i = 0; i < sChars.length; i++) {
+            if(sChars[i]!=tChars[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
