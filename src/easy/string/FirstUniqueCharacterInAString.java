3697857e21ecdb803b3335f9251d89731bf224a3
tangwanggong <tangwanggong@163.com>
2018-09-18 10:55:46 +0800

    2018-09-18

---
+++ src/easy/string/FirstUniqueCharacterInAString.java
@@ -0,0 +1,42 @@
+package easy.string;
+
+import java.util.Arrays;
+
+/**
+ * @author: Tang
+ * @create: 2018/9/18 9:33
+ **/
+public class FirstUniqueCharacterInAString {
+    /**
+     * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+     *
+     * Examples:
+     *
+     * s = "leetcode"
+     * return 0.
+     *
+     * s = "loveleetcode",
+     * return 2.
+     * Note: You may assume the string contain only lowercase letters.
+     * @param s
+     * @return
+     */
+    public static int firstUniqChar(String s) {
+        char[] chars = s.toCharArray();
+        int[] ints = new int[26];
+        for (int i = 0; i < chars.length; i++) {
+            ints[chars[i]-'a'] += 1;
+        }
+        int min = -1;
+        for (int i = 0; i < ints.length ; i++) {
+            if(ints[i]!=1){
+                continue;
+            }
+            int index = s.indexOf((char)(i+'a'));
+            min = min == -1?index:(min<index?min:index);
+        }
+        return min;
+    }
+
+
+}
---
+++ src/easy/tree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,44 @@
+package easy.tree;
+
+import structure.TreeNode;
+
+import javax.transaction.TransactionRequiredException;
+
+/**
+ * @author: Tang
+ * @create: 2018/9/18 10:15
+ **/
+public class ConvertSortedArrayToBinarySearchTree {
+    /**
+     * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+     *
+     * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+     *
+     * Example:
+     *
+     * Given the sorted array: [-10,-3,0,5,9],
+     *
+     * One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+     *
+     *       0
+     *      / \
+     *    -3   9
+     *    /   /
+     *  -10  5
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        TreeNode treeNode = toBeTree(nums,0,nums.length-1);
+        return treeNode;
+    }
+    public TreeNode toBeTree(int[] nums,int l,int r){
+        if(l>r)
+            return null;
+        int mid = (l+r)/2;
+        TreeNode treeNode = new TreeNode(nums[mid]);
+        treeNode.left = toBeTree(nums,l,mid-1);
+        treeNode.right = toBeTree(nums,mid+1,r);
+        return treeNode;
+    }
+}
