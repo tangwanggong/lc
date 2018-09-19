373694339609d7e098b0b47eceec6d8853842840
tangwanggong <tangwanggong@163.com>
2018-09-19 14:14:28 +0800

    2018-09-18

---
+++ src/easy/array/MissingNumber.java
@@ -0,0 +1,36 @@
+package easy.array;
+
+import java.util.Arrays;
+
+/**
+ * @author: Tang
+ * @create: 2018/9/19 8:49
+ **/
+public class MissingNumber {
+
+
+    /**
+     * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+     *
+     * Example 1:
+     *
+     * Input: [3,0,1]
+     * Output: 2
+     * Example 2:
+     *
+     * Input: [9,6,4,2,3,5,7,0,1]
+     * Output: 8
+     * @param nums
+     * @return
+     */
+    public static int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != i){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+}
