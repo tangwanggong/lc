88a3d06c477c733bc7a2a353c830f7bb91b46ce9
tangwanggong <tangwanggong@163.com>
2018-09-20 10:58:32 +0800

    2018-09-20

---
+++ src/easy/collection/MergeTwoSortedLists.java
@@ -0,0 +1,45 @@
+package easy.collection;
+
+import structure.ListNode;
+
+/**
+ * @author: Tang
+ * @create: 2018/9/20 9:24
+ **/
+public class MergeTwoSortedLists {
+
+    /**
+     * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+     *
+     * Example:
+     *
+     * Input: 1->2->4, 1->3->4
+     * Output: 1->1->2->3->4->4
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode(0);
+        merge(listNode,l1,l2);
+        return listNode.next;
+    }
+    public void merge(ListNode listNode,ListNode l1,ListNode l2){
+        if(l1 == null){
+            listNode.next = l2;
+            return;
+        }
+        if(l2 == null){
+            listNode.next = l1;
+            return;
+        }
+        if(l1.val<l2.val){
+            listNode.next = new ListNode(l1.val);
+            l1 = l1.next;
+        }else {
+            listNode.next = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+        merge(listNode.next,l1,l2);
+    }
+}
---
+++ src/easy/collection/PascalsTriangle.java
@@ -0,0 +1,52 @@
+package easy.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: Tang
+ * @create: 2018/9/20 10:25
+ **/
+public class PascalsTriangle {
+    /**
+     * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+     *
+     *
+     * In Pascal's triangle, each number is the sum of the two numbers directly above it.
+     *
+     * Example:
+     *
+     * Input: 5
+     * Output:
+     * [
+     *      [1],
+     *     [1,1],
+     *    [1,2,1],
+     *   [1,3,3,1],
+     *  [1,4,6,4,1]
+     * ]
+     * @param numRows
+     * @return
+     */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> lists = new ArrayList<>(numRows);
+        for (int i = 1;i<=numRows;i++){
+            List<Integer> list = new ArrayList<>(i);
+            if(i==1){
+                list.add(1);
+                lists.add(list);
+                continue;
+            }
+            List<Integer> lastList = lists.get(i-2);
+            for (int j = 0; j <lastList.size()+1 ; j++) {
+                if(j==0||j==lastList.size()){
+                    list.add(1);
+                }else {
+                    list.add(lastList.get(j-1)+lastList.get(j));
+                }
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+}
