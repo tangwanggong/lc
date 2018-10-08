9a31a78a9ac9159c07daa36e6d82d9408278ff3d
tangwanggong <tangwanggong@163.com>
2018-10-08 16:31:47 +0800

    2018-09-30

---
+++ src/easy/array/TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,40 @@
+package easy.array;
+
+/**
+ * @author: Tang
+ * @create: 2018/10/8 15:51
+ **/
+public class TwoSumIIInputArrayIsSorted {
+    /**
+     * Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+     *
+     * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+     *
+     * Note:
+     *
+     * Your returned answers (both index1 and index2) are not zero-based.
+     * You may assume that each input would have exactly one solution and you may not use the same element twice.
+     * Example:
+     *
+     * Input: numbers = [2,7,11,15], target = 9
+     * Output: [1,2]
+     * Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0,r = numbers.length-1;
+        while (l<=r){
+            if(numbers[l]+numbers[r]==target){
+                break;
+            }else if(numbers[l]+numbers[r]<target){
+                l++;
+            }else {
+                r--;
+            }
+        }
+        int[] result = new int[]{l++,r++};
+        return result;
+    }
+}
---
+++ src/easy/string/ValidPalindrome.java
@@ -0,0 +1,48 @@
+package easy.string;
+
+/**
+ * @author: Tang
+ * @create: 2018/10/8 16:10
+ **/
+public class ValidPalindrome {
+
+    /**
+     * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+     *
+     * Note: For the purpose of this problem, we define empty string as valid palindrome.
+     *
+     * Example 1:
+     *
+     * Input: "A man, a plan, a canal: Panama"
+     * Output: true
+     * Example 2:
+     *
+     * Input: "race a car"
+     * Output: false
+     * 转小写,根据具体的字符值进行判断,最后进行指针碰撞操作
+     * @param s
+     * @return
+     */
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        char[] chars = s.toCharArray();
+        int l = 0,r=chars.length-1;
+        while (l<=r){
+            if(('a'>chars[l]||chars[l]>'z')&&('0'>chars[l]||chars[l]>'9')){
+                l++;
+                continue;
+            }
+            if('a'>chars[r]||chars[r]>'z'&&('0'>chars[r]||chars[r]>'9')){
+                r--;
+                continue;
+            }
+            if(chars[l]!=chars[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+}
