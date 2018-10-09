e782d3b5cce356018184deed2ca0cfd5d44cf048
tangwanggong <tangwanggong@163.com>
2018-10-09 09:02:02 +0800

    2018-10-07

---
+++ src/easy/array/MinimumSizeSubarraySum.java
@@ -0,0 +1,46 @@
+package easy.array;
+
+/**
+ * @author: Tang
+ * @create: 2018/10/8 17:38
+ **/
+public class MinimumSizeSubarraySum {
+
+    /**
+     * Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+     *
+     * Example:
+     *
+     * Input: s = 7, nums = [2,3,1,2,4,3]
+     * Output: 2
+     * Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+     * Follow up:
+     * If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
+     *
+     * 利用滑动窗口的思想进行筛选
+     * @param s
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen(int s, int[] nums) {
+        int l = 0, r= -1;
+        int result = nums.length+1;
+        int sum = 0;
+        while (l<nums.length){
+            if(r+1<nums.length && sum<s){
+                r++;
+                sum += nums[r];
+            }else {
+                sum -= nums[l];
+                l++;
+            }
+            if(sum>=s){
+                result = Math.min(result,r-l+1);
+            }
+        }
+        if(result==nums.length+1){
+            result=0;
+        }
+        return result;
+    }
+}
---
+++ src/medium/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,48 @@
+package medium.string;
+
+/**
+ * @author: Tang
+ * @create: 2018/10/8 17:55
+ **/
+public class LongestSubstringWithoutRepeatingCharacters {
+    /**
+     * Given a string, find the length of the longest substring without repeating characters.
+     *
+     * Example 1:
+     *
+     * Input: "abcabcbb"
+     * Output: 3
+     * Explanation: The answer is "abc", with the length of 3.
+     * Example 2:
+     *
+     * Input: "bbbbb"
+     * Output: 1
+     * Explanation: The answer is "b", with the length of 1.
+     * Example 3:
+     *
+     * Input: "pwwkew"
+     * Output: 3
+     * Explanation: The answer is "wke", with the length of 3.
+     *              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+     * 使用一个计数数组进行每个字母出现次数的计数,如为0,则未出现,为1，则出现，使用滑动窗口的思想进行找出最大不同的数
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int l = 0, r= -1;
+        char[] chars = s.toCharArray();
+        int[] counts = new int[256];
+        int sum = 0;
+        while (l<chars.length){
+            if(r+1<chars.length&&counts[chars[r+1]]==0){
+                r++;
+                counts[chars[r]]++;
+            }else {
+                counts[chars[l]]--;
+                l++;
+            }
+            sum = Math.max(sum,r-l+1);
+        }
+        return sum;
+    }
+}
