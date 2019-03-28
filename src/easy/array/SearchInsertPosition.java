4e1cd23487197771e5874fbeb9b1a419bf098106
tangwanggong <tangwanggong@163.com>
2019-03-28 23:12:59 +0800

    2019-03-28

---
+++ src/easy/array/SearchInsertPosition.java
@@ -0,0 +1,45 @@
+package easy.array;
+
+/**
+ * @author: Tang
+ * @create: 2019/3/28 22:46
+ **/
+public class SearchInsertPosition {
+
+    /**
+     * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+     *
+     * You may assume no duplicates in the array.
+     *
+     * Example 1:
+     *
+     * Input: [1,3,5,6], 5
+     * Output: 2
+     * @param nums
+     * @param target
+     * @return
+     * 排过序的就想到二分查找,然后根据a[left]值返回响应的位置
+     */
+    public int searchInsert(int[] nums, int target) {
+        if(target<=nums[0]){
+            return 0;
+        }
+        int right = nums.length-1;
+        int left = 0;
+        while (left<right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                right = mid -1;
+            }else {
+                left = mid+1;
+            }
+        }
+        if(nums[left]<target){
+            return left+1;
+        }
+        return left;
+    }
+
+}
